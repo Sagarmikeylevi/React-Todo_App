45a02b41ed0fa938fc9651df142ebb1468e02ab0
unknown <sagarmikeylevi@gmail.com>
2023-04-24 22:26:10 +0530

    completed the project

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,51 +1,124 @@
-import { useEffect, useState } from 'react';
-import classes from './Todo.module.css';
-import TaskHeader from './taskHeader/TaskHeader';
-import TaskStorage from './tasks/TaskStorage';
+import { useEffect, useState } from "react";
+import classes from "./Todo.module.css";
+import TaskHeader from "./taskHeader/TaskHeader";
+import TaskStorage from "./tasks/TaskStorage";
 
 const Todo = () => {
-    const [tasks, setTasks] = useState([]);
-    useEffect(() => {
-        const fetchHandler = async () => {
-            const response = await fetch(
-              "https://jsonplaceholder.typicode.com/todos"
-            );
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchHandler = async () => {
+        setIsLoading(true);
+        setError(false);
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
 
-            const data = await response.json();
-
-            const taskDetails = data.slice(0, 3).map((task) => ({
-              key: task.id,
-              title: task.title,
-              checked: task.completed,
-            }));
-            
-            setTasks(taskDetails);
-            
+        if (!response.ok) {
+          throw new Error("Failed to fetch task data");
         }
 
-        fetchHandler();
-    }, []);
+        const data = await response.json();
 
-    const taskHandler = (title) => {
-        const taskDetails = {
-            key: Math.random().toString(),
-            title: title,
-            checked: false
-        }
-        console.log(taskDetails);
-        setTasks(prevState => [taskDetails, ...prevState]);
-    }
+        const taskDetails = data.slice(0, 3).map((task) => ({
+          key: task.id,
+          title: task.title,
+          checked: task.completed,
+        }));
 
-    const deleteHandler = (key) => {
-        const updatedTask = tasks.filter(task => task.key !== key);
-        setTasks(updatedTask);
-    }
+        setTasks(taskDetails);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+
+    fetchHandler();
+  }, []);
+
+
+  const taskHandler = async (title) => {
+    const taskDetails = {
+      key: Math.random().toString(),
+      title: title,
+      checked: false,
+    };
+
+    setTasks((prevState) => [taskDetails, ...prevState]);
+
+    // --- demo post req ----
+    // try {
+    //   const response = await fetch(
+    //     "https://jsonplaceholder.typicode.com/todos",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         title: taskDetails.title,
+    //         completed: taskDetails.checked,
+    //       }),
+    //       headers: {
+    //         "Content-type": "application/json; charset=UTF-8",
+    //       },
+    //     }
+    //   );
+
+    //   const data = await response.json();
+    //   return {
+    //     success: true,
+    //     data,
+    //   };
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  const deleteHandler = async (key) => {
+    const updatedTask = tasks.filter((task) => task.key !== key);
+    setTasks(updatedTask);
+
+    // --- demo delete req --- 
+    // try {
+    //   await fetch("https://jsonplaceholder.typicode.com/todos" + `/${key}`, {
+    //     method: "DELETE",
+    //   });
+    //   return {
+    //     success: true,
+    //   };
+    // } catch (error) {
+    //   return {
+    //     success: false,
+    //     message: error.message,
+    //   };
+    // }
+  };
+
+  if (isLoading) {
     return (
-      <div className={classes.wrapper}>
-        <TaskHeader onGetTaks={taskHandler} />
-        <TaskStorage tasks={tasks} onDelete={deleteHandler} />
-      </div>
+      <section className={classes.loadingstate}>
+        <p>Loading...</p>
+      </section>
     );
-}
+  }
+
+
+
+  if(error) {
+    return (
+      <section className={classes.errorstate}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
+
+  return (
+    <div className={classes.wrapper}>
+      <TaskHeader onGetTaks={taskHandler} />
+      <TaskStorage tasks={tasks} onDelete={deleteHandler} />
+    </div>
+  );
+};
 
 export default Todo;
