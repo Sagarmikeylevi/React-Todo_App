d20c00c8b2e25b76d36fbea68e71c33e72e6d75f
unknown <sagarmikeylevi@gmail.com>
2023-06-01 21:07:27 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Todo from "./components/Todo";
 
+// This line imports the Todo component from the "./components/Todo" file.
+
 function App() {
+  // This is the main function/component called App.
+  // It returns the JSX code that will be rendered on the screen.
+
   return (
     <>
+      {/* This is a JSX fragment that wraps the Todo component. */}
       <Todo />
     </>
   );
 }
 
 export default App;
+// This line exports the App component so that it can be imported and used in other files.
